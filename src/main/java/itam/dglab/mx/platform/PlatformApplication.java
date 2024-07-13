deb63454abeae44b21eb0c0913562bb55996ecf0
Miguel Quintero <maquinterov0399@gmail.com>
2024-07-13 13:03:41 -0700

    Initial commit

---
+++ src/main/java/itam/dglab/mx/platform/PlatformApplication.java
@@ -0,0 +1,13 @@
+package itam.dglab.mx.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itam/dglab/mx/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package itam.dglab.mx.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
